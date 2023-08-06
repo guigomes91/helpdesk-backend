e1d53dcfbc727ec48ffd3d6171e5a5297997c8ac
guigomes91 <guilherme.gomes91@outlook.com>
2023-08-06 19:23:44 -0300

    endpoint para update de chamado

--- src/main/java/com/gomes/helpdesk/services/ChamadoService.java
+++ src/main/java/com/gomes/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.gomes.helpdesk.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -41,14 +42,27 @@ public class ChamadoService {
 	public Chamado create(@Valid ChamadoDTO objDTO) {
 		return repository.save(newChamado(objDTO));
 	}
+	
+	public Chamado update(Integer id, @Valid ChamadoDTO objDTO) {
+		objDTO.setId(id);
+		Chamado oldObj = findById(id);
+		oldObj = newChamado(objDTO);
+		
+		return repository.save(oldObj);
+	}
 
 	private Chamado newChamado(ChamadoDTO obj) {
 		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
 		Cliente cliente = clienteService.findById(obj.getCliente());
 
 		Chamado chamado = new Chamado();
+		
 		if (obj.getId() != null) {
 			chamado.setId(obj.getId());
+		}
+		
+		if (obj.getStatus().equals(2)) {
+			chamado.setDataFechamento(LocalDate.now());
 		}
 
 		chamado.setTecnico(tecnico);
