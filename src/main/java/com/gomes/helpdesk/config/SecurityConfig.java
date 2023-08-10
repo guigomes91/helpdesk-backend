f6e00d4ef3da9b9426d48c18a4bd279c084caa3a
guigomes91 <guilherme.gomes91@outlook.com>
2023-08-09 21:16:12 -0300

    Ajuste classe SecurityConfig

--- src/main/java/com/gomes/helpdesk/config/SecurityConfig.java
+++ src/main/java/com/gomes/helpdesk/config/SecurityConfig.java
@@ -1,31 +1,41 @@
 package com.gomes.helpdesk.config;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 	
+	@Autowired
+	private Environment env;
+	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers((headers) -> headers.disable());
+		}
+		
 		return http
 		        .csrf(csrf -> csrf.disable())
 		        .authorizeHttpRequests(auth -> auth
-		            .requestMatchers(antMatcher("/token/**", "/h2-console/**"))
+		            .requestMatchers(new AntPathRequestMatcher("/h2-console/**"))
 		            .permitAll()
 		            .anyRequest()
 		            .authenticated()
 		        )
 		        .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-		        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
 		        .httpBasic(Customizer.withDefaults())
 		        .build();
 	}
